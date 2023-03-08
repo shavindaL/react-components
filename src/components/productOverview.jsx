96d4bacdc9b5dd0de18a928e67144cc036d1e31d
shavindaL <shavinda724@gmail.com>
2023-03-08 10:35:48 +0530

    Product Overview Component 2.0

--- src/components/productOverview.jsx
+++ src/components/productOverview.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import StarRating from "./starRating";
 
 export default function ProductOverview() {
@@ -8,8 +8,10 @@ export default function ProductOverview() {
     "https://picsum.photos/id/10/300",
     "https://picsum.photos/id/237/300",
   ];
-  const productName = "Dhammika Syrup";
+  const productName = "ABC Balm";
   const reviewCount = 69;
+  const price = 12;
+  const stock = 20;
   const productSpecification = [
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ultrices.",
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ultrices.",
@@ -21,6 +23,9 @@ export default function ProductOverview() {
 
   const [mainImg, setMainImg] = useState(imgLinks[0]);
   const [count, setCount] = useState(0);
+  const [total, setTotal] = useState(0);
+  useEffect(() => setTotal(price * count), [count]);
+
   return (
     <div className="bg-white p-16 grid grid-cols-3 gap-4">
       <div className="">
@@ -39,7 +44,7 @@ export default function ProductOverview() {
               >
                 <img
                   src={link}
-                  className="w-44 px-1"
+                  className="w-44 px-1 hover:rounded-lg"
                   onClick={() => setMainImg(imgLinks[idx])}
                 />
               </div>
@@ -47,11 +52,29 @@ export default function ProductOverview() {
           </div>
         </div>
       </div>
+
       <div>
+        {/* Product Details Section */}
         <div className="w-210 h-auto border-green-9 border-solid border-2 rounded-lg relative p-8">
-          <h1 className="text-4xl font-700">{productName}</h1>
-          <StarRating />
-          {`${reviewCount} reviews`}
+          <table>
+            <tbody>
+              <tr>
+                <td>
+                  <h1 className="text-4xl font-700">{productName}</h1>
+                </td>
+                <td>
+                  <p className="ml-8 text-3xl font-700">{`$ ${price} /each`}</p>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <StarRating />
+                  <p className="inline-block px-2">{`${reviewCount} reviews`}</p>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+
           <ul className="px-12">
             {productSpecification.map((specificattion, idx) => (
               <li className="list-disc" key={idx}>
@@ -60,27 +83,69 @@ export default function ProductOverview() {
             ))}
           </ul>
         </div>
+        {/* Product Details Section ends here*/}
+
+        {/* Product Count and total */}
         <div className="w-210 h-auto border-green-9 border-solid border-2 rounded-lg my-8">
-          <div className="border-solid rounded-full border-green-9 border-2 inline-block p-1 m-2" onClick={()=>setCount(count+1)}>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 448 512"
-              width={20}
-            >
-              {/*Font Awesome Pro 6.3.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc.*/}
-              <path d="M240 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H32c-17.7 0-32 14.3-32 32s14.3 32 32 32H176V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H384c17.7 0 32-14.3 32-32s-14.3-32-32-32H240V80z" />
-            </svg>
-          </div>
-          <div className="inline-block text-4xl">{count}</div>
-          <div className="border-solid rounded-full border-green-9 border-2 inline-block p-1  m-2" onClick={()=>count>0?setCount(count-1):setCount(0)}>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 448 512"
-              width={20}
-            >
-              <path d="M416 256c0 17.7-14.3 32-32 32L32 288c-17.7 0-32-14.3-32-32s14.3-32 32-32l352 0c17.7 0 32 14.3 32 32z" />
-            </svg>
-          </div>
+          <table>
+            <tbody>
+              <tr>
+                <td>
+                  <div
+                    className="border-solid rounded-full border-green-9 border-2 inline-block p-1  m-2 hover:cursor-default hover:bg-green-4 hover:fill-white"
+                    onClick={() => {
+                      count > 0 ? setCount(count - 1) : setCount(0);
+                    }}
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 448 512"
+                      width={20}
+                    >
+                      <path d="M416 256c0 17.7-14.3 32-32 32L32 288c-17.7 0-32-14.3-32-32s14.3-32 32-32l352 0c17.7 0 32 14.3 32 32z" />
+                    </svg>
+                  </div>
+                </td>
+                <td>
+                  <input
+                    className="inline-block text-4xl border-solid  border-green-9 border-2 px-2 rounded-md w-24 text-center"
+                    value={count}
+                    onChange={(e) => {
+                      setCount(e.target.value);
+                    }}
+                  />
+                </td>
+                <td>
+                  <div
+                    className="border-solid rounded-full border-green-9 border-2 inline-block p-1 m-2 hover:cursor-default hover:bg-green-4 hover:fill-white"
+                    onClick={() => (stock > count ? setCount(count + 1) : null)}
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 448 512"
+                      width={20}
+                    >
+                      {/*Font Awesome Pro 6.3.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc.*/}
+                      <path d="M240 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H32c-17.7 0-32 14.3-32 32s14.3 32 32 32H176V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H384c17.7 0 32-14.3 32-32s-14.3-32-32-32H240V80z" />
+                    </svg>
+                  </div>
+                </td>
+                <td className="text-4xl w-max" width={2}>
+                  <p className="ml-8 inline-block">{`Total : $ ${total}`}</p>
+                </td>
+                <td className="px-8">
+                  <button className="text-white bg-green-7 border-solid border-2 px-5 py-2 rounded-lg hover:border-green-7 hover:text-green-7 hover:bg-white">
+                    Buy Now
+                  </button>
+                </td>
+                <td className="px-8">
+                  <button className="text-white bg-green-7 border-solid border-2 px-5 py-2 rounded-lg hover:border-green-7 hover:text-green-7 hover:bg-white">
+                    Add to cart
+                  </button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
         </div>
       </div>
     </div>
